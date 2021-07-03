318b1ed4f6156dd9c01bc00de014430ba592b71a
arman-ahmed-jony <armanahmedjony786@gmail.com>
2021-07-04 02:16:20 +0600

    refacotor index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const app = express()
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 
+app.use(express.json())
+app.use(helmet())
+app.use(express.static(path.join(__dirname, 'static')))
+app.set('view engine', 'pug')
+
 const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 })
@@ -17,11 +22,17 @@ io.on('connection', (socket) => {
   console.log('a user connected', socket)
 })
 
+/**
+ * jwt private key test, wheter it's available in env
+ */
 if (!process.env.jwtPrivateKey) {
   console.log('FATAL: Json web token is not defined ')
   process.exit(1)
 }
 
+/**
+ * database connetion test
+ */
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -32,16 +43,11 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(`[application] ${error}`))
 db.once('open', () => console.log('[application] connected to database'))
 
-app.use(express.json())
-app.use(helmet())
-app.use(express.static(path.join(__dirname, 'static')))
-app.set('view engine', 'pug')
-
-server.listen(4000, () =>
-  console.log(`[application] server created in port ${4000}... `)
-)
-
 /**
  * providing routes to the application
  */
 require('./routes')(app)
+
+server.listen(4000, () =>
+  console.log(`[application] server created in port ${4000}... `)
+)
