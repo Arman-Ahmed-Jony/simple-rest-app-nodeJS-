01d7bedd77ab29215c27134049d0aee46dabbe0d
arman-ahmed-jony <armanahmedjony786@gmail.com>
2021-07-04 01:41:56 +0600

    added index file for chat app and main.js along with bundeller

--- static/main.js
+++ static/main.js
@@ -1,1 +1,7 @@
-console.log('js called')
+// new WebSocket(`ws://${window.location.hostname}:4000`)
+
+const { io } = require('socket.io-client')
+
+const socket = io('ws://localhost:4000', { transport: ['websocket'] })
+
+console.log(socket)
